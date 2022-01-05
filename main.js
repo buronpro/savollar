1b08bb40d69829b1a4c73948d32347f655f8b7b6
buronpro <buronbek33@gmail.com>
2022-01-05 11:08:41 +0500

    savollar

---
+++ main.js
@@ -0,0 +1,20 @@
+let Name = prompt ("ismingiz😇");
+let NameFam  = prompt ("Familyangiz😇");
+let NameYosh = prompt ("Yoshingiz😇");
+let NameNamber = prompt ("Nomeriz😇");
+let NameIsh = prompt ("ishngiz😇") ;
+let NameMosh = prompt ("Qanaqa moshina yoqadi😇")
+ 
+
+
+let javob = [
+    "ismingiz☺️:"+" "+ Name + "\n"+
+    "Familyangiz😊:"+" "+ NameFam +"\n" +
+    "Yoshingiz🤞:"+" "+ NameYosh + "\n" +
+    "Nomeriz🙂:"+" "+ NameNamber + "\n" +
+    "Ishingiz🤪:" +" "+ NameIsh + "\n" +
+    "Qanaqa moshina yoqadi🚗:"+ " "+ NameMosh + "\n" 
+
+]
+
+alert (javob)
